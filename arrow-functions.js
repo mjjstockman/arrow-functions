43f88dbe6a84d2dabc77f7135c75d16ef4f14ea6
mjjstockman <56722629+mjjstockman@users.noreply.github.com>
2021-12-03 11:05:19 +0000

    Add additional example

--- arrow-functions.js
+++ arrow-functions.js
@@ -45,3 +45,40 @@ const returnMultipleLines = () => (
 )
 
 console.log(returnMultipleLines());
+
+
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'maths','english', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', english: 'Joan', art: 'Simon'},
+        results: {science: 93, maths: 95, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 79, art: 95},
+    }
+];
+
+const averagePoints = (arr, subject) => {
+    totalPoints = 0;
+    numOfStudents = 0;
+    for (let itm of arr) {
+        if (itm.subjects.includes(subject)) {
+            totalPoints += itm.results[subject];
+            numOfStudents += 1;
+        }
+    }
+    return totalPoints / numOfStudents;
+}
+
+let result = averagePoints(students, 'art');
+console.log(result);
